ae45df4fbce13f210b30cf9336261b571a969a3e
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-12-05 08:56:27 +0600

    some feature added

--- src/Pages/Updates/Updates.jsx
+++ src/Pages/Updates/Updates.jsx
@@ -2,9 +2,9 @@ import Marquee from "react-fast-marquee";
 
 const Updates = () => {
     return (
-        <div className="bg-[#2B3440] py-2 px-5">
-            <div className="flex">
-                <button className="btn bg-[#13A2B7] px-10 text-white hover:bg-slate-900 rounded-lg">Updates</button>
+        <div className="bg-[rgb(43,52,64)] py-2 px-5">
+            <div className="flex ">
+                <button className="px-2 lg:px-8  bg-[#13A2B7] lg:bg-[#13A2B7] py-2  hover:bg-[#99dae4]  rounded-lg text-white border border-white ">Updates</button>
                 <Marquee className="text-white" speed={50} pauseOnHover>
                     <p> Voluptates, minus! ||  Lorem ipsum dolor sit amet....   <span className="me-12"></span>Lorem ipsum dolor sit amet.</p>
                    
